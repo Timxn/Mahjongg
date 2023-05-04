83bf3add71665ddf309b62b5a51730ede85e8f4a
Timon <stronczek.timon@gmail.com>
2023-05-05 00:11:58 +0200

    Test

---
+++ app/src/main/java/de/timon/mahjongg/ui/history/HistoryFragment.java
@@ -0,0 +1,37 @@
+package de.timon.mahjongg.ui.history;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import de.timon.mahjongg.databinding.FragmentHistoryBinding;
+
+public class HistoryFragment extends Fragment {
+
+    private FragmentHistoryBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HistoryViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(HistoryViewModel.class);
+
+        binding = FragmentHistoryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textNotifications;
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/de/timon/mahjongg/ui/history/HistoryViewModel.java
@@ -0,0 +1,19 @@
+package de.timon.mahjongg.ui.history;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HistoryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HistoryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is history fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
