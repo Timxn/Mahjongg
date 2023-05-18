f554d756f25084efdc5f1396a91bb492a7d6e151
Timon <stronczek.timon@gmail.com>
2023-05-18 15:37:38 +0200

    all important data will now be saved in the model

--- app/src/main/java/de/timon/mahjongg/ui/score/ScoreViewModel.java
+++ app/src/main/java/de/timon/mahjongg/ui/score/ScoreViewModel.java
@@ -6,14 +6,20 @@ import androidx.lifecycle.ViewModel;
 
 public class ScoreViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    public boolean gameInProgress = false;
+    public String[] names = new String[4];
+    public int east = -1;
+    public int currentRound = 0;
+    public int playerCount = 4;
+    public int [] [] scores = new int[4][2];
+    public boolean tempValue = false;
+    public View view = View.NEWGAME;
 
-    public ScoreViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is score fragment");
+    protected enum View {
+        INGAME,
+        NEWGAME,
+        INPUTNAMES
     }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
+    //q: which is for row, which is for column of the []
+    //a: first [] is for row, second [] is for column
 }
